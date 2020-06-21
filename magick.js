a8113d692d3b68e11621cc470d2f03ae42fefd85
K Adam White <kadamwhite@users.noreply.github.com>
2020-06-21 18:24:10 -0400

    Chage greyscale isJPG check to ! isPNG; fix indentation

--- magick.js
+++ magick.js
@@ -33,7 +33,7 @@ async function isBW( absFilePath ) {
  * @param {String} absFilePath Absolute file system path to an image.
  */
 const convertToGreyscale = async ( absFilePath ) => {
-	const isJPG = /\.jpg/i.test( absFilePath );
+	const isPNG = /\.png/i.test( absFilePath );
 
 	const tmpFile = replaceExtension( absFilePath, '.tmp.png' );
 	const optimizedFile = replaceExtension( absFilePath, '.tmp.opt.png' );
@@ -41,29 +41,29 @@ const convertToGreyscale = async ( absFilePath ) => {
 
 	// https://imagemagick.org/script/command-line-options.php
 	await spawn( 'convert', [
-			absFilePath,
-			'-alpha', 'off',
-			'-interlace', 'none',
-			'-colors', '17',
-			// Reduce color depth in highs and lows for better text contrast.
-			'-white-threshold', '95%',
-			'-black-threshold', '10%',
-			// Lighten the image for better display on Kindle screens.
-			'-sigmoidal-contrast', '3x10%', // This could be refined further.
-			'-gamma', '1.1', // Ditto. This *and* sigmoidal contrast...? Bad?
-			tmpFile,
+		absFilePath,
+		'-alpha', 'off',
+		'-interlace', 'none',
+		'-colors', '17',
+		// Reduce color depth in highs and lows for better text contrast.
+		'-white-threshold', '95%',
+		'-black-threshold', '10%',
+		// Lighten the image for better display on Kindle screens.
+		'-sigmoidal-contrast', '3x10%', // This could be refined further.
+		'-gamma', '1.1', // Ditto. This *and* sigmoidal contrast...? Bad?
+		tmpFile,
 	] );
 
 	// https://pngquant.org/
 	await spawn( 'pngquant', [
-			'--ext', '.opt.png',
-			'--speed', '1',
-			tmpFile,
+		'--ext', '.opt.png',
+		'--speed', '1',
+		tmpFile,
 	] );
 
 	// Clean up.
-	if ( isJPG ) {
-			await spawn( 'rm', [ absFilePath ] );
+	if ( ! isPNG ) {
+		await spawn( 'rm', [ absFilePath ] );
 	}
 	await spawn( 'rm', [ tmpFile ] );
 	await spawn( 'mv', [ optimizedFile, targetFile ] );
@@ -76,31 +76,31 @@ const convertToGreyscale = async ( absFilePath ) => {
  * @param {String} absFilePath Absolute file system path to an image.
  */
 const convertToColor = async ( absFilePath ) => {
-	const isJPG = /\.jpg/i.test( absFilePath );
+	const isJPG = /\.jpe?g/i.test( absFilePath );
 
 	const tmpFile = replaceExtension( absFilePath, '.tmp.jpg' );
 	const targetFile = replaceExtension( absFilePath, '.jpg' );
 
 	// https://imagemagick.org/script/command-line-options.php
 	await spawn( 'convert', [
-			absFilePath,
-			'-interlace', 'none',
-			'-quality', '80',
-			'-strip', // No ICC profiles.
-			tmpFile,
+		absFilePath,
+		'-interlace', 'none',
+		'-quality', '80',
+		'-strip', // No ICC profiles.
+		tmpFile,
 	] );
 	// https://www.kokkonen.net/tjko/src/man/jpegoptim.txt
 	await spawn( 'jpegoptim', [
-			tmpFile,
-			'-m', '80', // Max quality 80.
-			'--strip-icc',
-			'--strip-exif',
-			'--all-normal',
+		tmpFile,
+		'-m', '80', // Max quality 80.
+		'--strip-icc',
+		'--strip-exif',
+		'--all-normal',
 	] );
 
 	// Clean up.
 	if ( ! isJPG ) {
-			await spawn( 'rm', [ absFilePath ] );
+		await spawn( 'rm', [ absFilePath ] );
 	}
 	await spawn( 'mv', [ tmpFile, targetFile ] );
 };
@@ -114,9 +114,9 @@ const convertToColor = async ( absFilePath ) => {
  */
 const optimize = async ( absFilePath ) => {
 	if ( await isBW( absFilePath ) ) {
-			await convertToGreyscale( absFilePath );
+		await convertToGreyscale( absFilePath );
 	} else {
-			await convertToColor( absFilePath );
+		await convertToColor( absFilePath );
 	}
 };
 
